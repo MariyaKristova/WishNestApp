cde9fe93a46b61a6829641f95fc579792dd2cb16
MariyaKristova <maria.vinogradova@gmail.com>
2024-11-15 13:41:11 +0200

    update custom user logic

--- WishNestApp/accounts/models/customuser_model.py
+++ WishNestApp/accounts/models/customuser_model.py
@@ -2,7 +2,7 @@ from django.contrib.auth.base_user import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
 from django.db import models
 
-from wishnest_app.accounts.managers import CustomUserManager
+from WishNestApp.accounts.managers import CustomUserManager
 
 
 class CustomUser(AbstractBaseUser, PermissionsMixin):
