ebe4677996abc9b242454a86ccb0f28751f2c1a0
MariyaKristova <maria.vinogradova@gmail.com>
2024-11-15 14:16:41 +0200

    create login logic

--- WishNestApp/accounts/admin.py
+++ WishNestApp/accounts/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin
-
 from WishNestApp.accounts.forms import UserCreateForm, UserEditForm
 
 UserModel = get_user_model()
@@ -12,13 +11,13 @@ class CustomUserAdmin(UserAdmin):
     add_form = UserCreateForm
     form = UserEditForm
 
-    # list_display = ('pk', 'email', 'is_staff', 'is_superuser')
-    # search_fields = ('email',)
-    # ordering = ('pk',)
-    #
-    # fieldsets = ((None, {'fields': ('email', 'password')}), ('Personal info', {'fields': ()}),
-    #              ('Permissions', {'fields': ('is_active', 'is_staff', 'groups', 'user_permissions')}),
-    #              ('Important dates', {'fields': ('last_login',)}),)
-    #
-    # add_fieldsets = ((None, {"classes": ("wide",),
-    #                          "fields": ("email", "password1", "password2"), }))
+    list_display = ('pk', 'email', 'is_staff', 'is_superuser')
+    search_fields = ('email',)
+    ordering = ('pk',)
+
+    fieldsets = ((None, {'fields': ('email', 'password')}), ('Personal info', {'fields': ()}),
+                 ('Permissions', {'fields': ('is_active', 'is_staff', 'groups', 'user_permissions')}),
+                 ('Important dates', {'fields': ('last_login',)}),)
+
+    add_fieldsets = ((None, {"classes": ("wide",),
+                             "fields": ("email", "password1", "password2"), }))
