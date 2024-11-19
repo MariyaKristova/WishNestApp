adbde6cb643546262db0ffa142cb6a8079b7f8ad
MariyaKristova <maria.vinogradova@gmail.com>
2024-11-20 00:56:21 +0200

    work on share functionality

--- WishNestApp/common/models.py
+++ WishNestApp/common/models.py
@@ -1,5 +1,7 @@
+import uuid
 from django.db import models
-
+from django.utils import timezone
+from datetime import timedelta
 from WishNestApp.events.models import Event
 
 
@@ -8,3 +10,21 @@ class Hug(models.Model):
     text = models.TextField(max_length=100)
     date_of_publication = models.DateTimeField(auto_now_add=True)
     to_event = models.ForeignKey(Event, on_delete=models.CASCADE, related_name='hugs')
+
+
+class ShareLink(models.Model):
+    event = models.ForeignKey(Event, on_delete=models.CASCADE, related_name='share_links')
+    token = models.UUIDField(default=uuid.uuid4, unique=True, editable=False)
+    created_at = models.DateField(auto_now_add=True)
+    expires_at = models.DateField()
+
+    def is_valid(self):
+        return timezone.now() <= self.expires_at
+    #
+    # def save(self, *args, **kwargs):
+    #     if not self.expires_at:
+    #         self.expires_at = timezone.now() + timedelta(days=7)
+    #     super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f"Share link for {self.event.occasion}"
