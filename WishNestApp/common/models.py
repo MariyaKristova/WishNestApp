6e6c5a3c627570db6c5d42723eaf00e31dcfbfae
MariyaKristova <maria.vinogradova@gmail.com>
2024-11-29 11:47:41 +0200

    update share link logic

--- WishNestApp/common/models.py
+++ WishNestApp/common/models.py
@@ -20,11 +20,11 @@ class ShareLink(models.Model):
 
     def is_valid(self):
         return timezone.now() <= self.expires_at
-    #
-    # def save(self, *args, **kwargs):
-    #     if not self.expires_at:
-    #         self.expires_at = timezone.now() + timedelta(days=7)
-    #     super().save(*args, **kwargs)
+
+    def save(self, *args, **kwargs):
+        if not self.expires_at:
+            self.expires_at = self.event.date + timedelta(days=7)
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return f"Share link for {self.event.occasion}"
